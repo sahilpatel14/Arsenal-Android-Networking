2359c22c2c09eb25db113218822e34cbd2bd20a9
Sahil Patel <sahilpatel1410@gmail.com>
2018-06-13 10:47:53 +0530

    some changes to ui

---
+++ app/src/main/java/com/ninthsemester/arsenalandroidnetworking/login/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.ninthsemester.arsenalandroidnetworking.login
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.ninthsemester.arsenalandroidnetworking.R
+import com.ninthsemester.arsenalandroidnetworking.api.ConnectionDetector
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
